9eea425380ff8c79d8d8b63b5b9c28e4e470055f
miryanakv <mirjana.krneta@gmail.com>
2021-08-10 00:03:59 +0200

    react files added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# cocktail-search
-## App in react 
+#### React Router Fix
+
+(Fix)[https://dev.to/dance2die/page-not-found-on-netlify-with-react-router-58mc]
+
+#### CRA Fix
+
+```
+
+"build": "CI= react-scripts build",
+
+```
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section-about-section'>
+      <h1 className='section-title'>about us</h1>
+        <p>ksjvsddsdjfsdvskvperferendis eos, autem
+        maiores nisi saepe quisquam hic odio consectetur
+        nobis veritatis quasi explicabo obcaecati
+        doloremque? Placeat ratione
+        hic aspernatur error blanditiis?</p>
+    </section>
+  )
+}
+
+export default About
